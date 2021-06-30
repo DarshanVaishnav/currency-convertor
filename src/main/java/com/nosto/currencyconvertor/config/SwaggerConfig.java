3d42df9aa970a357c1c03f5a5e0994b4bb9b8784
darshanMT <darshan.vaishnav@mindtickle.com>
2021-06-30 23:37:41 +0530

    added SwaggerConfig

---
+++ src/main/java/com/nosto/currencyconvertor/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.nosto.currencyconvertor.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Currency-Convertor")
+                                .description("Nosto's Currency-Convertor Application")
+                                .version("v1.0"));
+    }
+}
